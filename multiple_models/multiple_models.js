06714cd8ad405fbcc2f4d0b8b24535f4eb266cd8
Josh <joshsimon85@yahoo.com>
2019-12-27 09:24:41 -0700

    Finish backbone models demo

--- multiple_models/multiple_models.js
+++ multiple_models/multiple_models.js
@@ -1,8 +1,14 @@
 $('#items_json').remove();
 
-const ItemModel = Backbone.Model.extend();
+const ItemModel = Backbone.Model.extend({
+  initialize: function() {
+    this.set('id', this.cid);
+  }
+});
+
 const ItemsCollection = Backbone.Collection.extend({
   model: ItemModel,
+  comparator: 'name',
 });
 
 let item1 = new ItemModel(items_json[0]);
@@ -15,18 +21,56 @@ const app = {
   templates: {},
   createTemplates: function() {
     var self = this;
-    $('[type="text/x-handlebars"]').each(function(tmp) {
-      self.templates[$(this).attr('id')] = Handlebars.compile($(this).html());
-      $(this).remove();
+    $('[type="text/x-handlebars"]').each(function(idx) {
+      if (idx == 0) {
+        self.templates[$(this).attr('id')] = Handlebars.compile($(this).html());
+        $(this).remove();
+      } else {
+        self.templates[$(this).attr('id')] = Handlebars.compile($(this).html());
+        Handlebars.registerPartial('item', $(this).html());
+      }
     });
   },
   renderItems: function() {
-    debugger;
-    $table.append(this.templates.items(items.toJSON()));
+    $table.children().remove();
+    $table.append(this.templates.items({items: items}));
+  },
+  renderItem: function(item) {
+    $table.append(this.templates.item(item.toJSON()));
+  },
+  createItem: function(e) {
+    e.preventDefault();
+    let args = {};
+    let vals = $('form').serializeArray();
+    vals.forEach(function(pair) {
+      args[pair['name']] = pair['value']
+    });
+
+    let item = new ItemModel(args);
+    items.add(item);
+    this.renderItems()
+    document.querySelector('form').reset();
+  },
+  deleteItem: function(e) {
+    e.preventDefault();
+    let id = $(e.target).attr('data-id');
+    items.remove(id);
+    this.renderItems();
+  },
+  removeAll: function(e) {
+    e.preventDefault();
+    items.reset();
+    this.renderItems();
+  },
+  bindEvents: function() {
+    $('form').on('submit', this.createItem.bind(this));
+    $('table tbody').on('click', 'a', this.deleteItem.bind(this));
+    $('table + p').on('click', this.removeAll.bind(this));
   },
   init: function() {
     this.createTemplates();
     this.renderItems();
+    this.bindEvents();
   }
 };
 
